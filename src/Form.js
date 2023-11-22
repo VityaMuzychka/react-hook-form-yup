87d3e2cab7ea4c1b3da8d817c66670ef80d9e8b9
VityaMuzychka <mirrorvitya@gmail.com>
2023-11-22 23:02:55 +0200

    added useForm hook

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,14 @@
+import {useForm} from 'react-hook-form';
+
 export const Form = () => {
-    
+ const {register, handleSubmit} = useForm();
+
+ const onSubmit = () => {
+   console.log('HELLO WORLD');
+ }
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit(onSubmit)}>
             <input type="text" placeholder="Full Name..." />
             <input type="text" placeholder="Email..." />
             <input type="number" placeholder="Age..." />
