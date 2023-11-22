6a0a6fc2718e48968e6a0cdfdb9ad1870ce04ea1
VityaMuzychka <mirrorvitya@gmail.com>
2023-11-22 22:37:31 +0200

    added component, inputs and css

---
+++ src/Form.js
@@ -0,0 +1,13 @@
+export const Form = () => {
+    
+    return (
+        <form className="form">
+            <input type="text" placeholder="Full Name..." />
+            <input type="text" placeholder="Email..." />
+            <input type="number" placeholder="Age..." />
+            <input type="password" placeholder="Password..." />
+            <input type="password" placeholder="Confirm Password..." />
+            <input type="submit" />
+        </form>
+    )
+}
