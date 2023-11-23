78c797018f746ea5fc121e2d228f30a7f7a68adb
VityaMuzychka <mirrorvitya@gmail.com>
2023-11-23 13:14:54 +0200

    register is implemented into the inputs

--- src/Form.js
+++ src/Form.js
@@ -1,19 +1,20 @@
-import {useForm} from 'react-hook-form';
+import { useForm } from "react-hook-form"
+
 
 export const Form = () => {
- const {register, handleSubmit} = useForm();
+  const {register, handleSubmit} = useForm();
 
- const onSubmit = () => {
-   console.log('HELLO WORLD');
- }
+  const onSubmit = (data) => {
+   console.log(data);
+  }
 
     return (
         <form className="form" onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" placeholder="Full Name..." />
-            <input type="text" placeholder="Email..." />
-            <input type="number" placeholder="Age..." />
-            <input type="password" placeholder="Password..." />
-            <input type="password" placeholder="Confirm Password..." />
+            <input type="text" placeholder="Full Name..." {...register('fullName')} />
+            <input type="text" placeholder="Email..." {...register('email')} />
+            <input type="number" placeholder="Age..." {...register('age')} />
+            <input type="password" placeholder="Password..." {...register('password')} />
+            <input type="password" placeholder="Confirm Password..." {...register('confirmPassword')} />
             <input type="submit" />
         </form>
     )
